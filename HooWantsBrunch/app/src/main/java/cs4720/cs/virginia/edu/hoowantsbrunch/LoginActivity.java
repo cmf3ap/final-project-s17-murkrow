a7bcd0a0e9534e14f8ce851c4ae22bc6b69b3c6e
Samantha Chu <syc5pg@virginia.edu>
2017-04-17 16:12:36 -0400

    Starting login

--- HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/LoginActivity.java
+++ HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/LoginActivity.java
@@ -3,11 +3,16 @@ package cs4720.cs.virginia.edu.hoowantsbrunch;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
 
 import android.content.CursorLoader;
 import android.content.Loader;
@@ -327,13 +332,22 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             return true;
         }
 
+
+       /* public void openMain(View view){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }*/
+
         @Override
+
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
             showProgress(false);
 
             if (success) {
-                finish();
+
+
+                //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
