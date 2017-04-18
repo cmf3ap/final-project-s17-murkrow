d4aa64b54ef8a5ef09a70fc274ddf43c6aa0e54f
Claire Frank <cmf3ap@virginia.edu>
2017-04-17 20:22:47 -0400

    We got 3 buttons

--- HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/MainActivity.java
+++ HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/MainActivity.java
@@ -2,12 +2,42 @@ package cs4720.cs.virginia.edu.hoowantsbrunch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addReview;
+    private Button findRestaurants;
+    private Button viewRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addReview = (Button) findViewById(R.id.addReview);
+        addReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+            }
+        });
+        viewRestaurants = (Button) findViewById(R.id.viewRestaurants);
+        viewRestaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+            }
+        });
+        findRestaurants = (Button) findViewById(R.id.findRestaurant);
+        findRestaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+            }
+        });
+
     }
+
 }
