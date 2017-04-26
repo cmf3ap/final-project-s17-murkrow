c663d8dc83c836ce8b3eaeeeee2f5cac87be7300
Samantha Chu <syc5pg@virginia.edu>
2017-04-26 13:33:21 -0400

    Submitting review stores to SQLite database

--- HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/ViewRestaurants.java
+++ HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/ViewRestaurants.java
@@ -1,13 +1,35 @@
 package cs4720.cs.virginia.edu.hoowantsbrunch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewRestaurants extends AppCompatActivity {
+
+    TextView reviewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_restaurants);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        reviewText = (TextView)findViewById(R.id.ReviewContent);
+
+        reviewText.setText(userReview.review);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
