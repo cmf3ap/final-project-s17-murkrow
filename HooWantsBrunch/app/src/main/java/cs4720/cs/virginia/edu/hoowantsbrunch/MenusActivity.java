8c530ef2a0a778e4a69e46e7464cf2cd5d4cc713
Claire Frank <cmf3ap@virginia.edu>
2017-04-26 23:15:52 -0400

    Map Feature now tells the user the closest restaurant at the top

--- HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/MenusActivity.java
+++ HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/MenusActivity.java
@@ -4,16 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class MenusActivity extends AppCompatActivity {
 
     private ImageButton backToMain;
+    Spinner spinner;
+    String restaurant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menus);
+
+        spinner = (Spinner) findViewById(R.id.menus_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.restaurants_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         backToMain = (ImageButton) findViewById(R.id.backMenu);
         backToMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +41,4 @@ public class MenusActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-    // fixed a thing
 }
