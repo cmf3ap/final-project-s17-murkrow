72d79830bf9469c0d0db63f8f31a63936ceac2e5
Claire Frank <cmf3ap@virginia.edu>
2017-04-26 23:53:34 -0400

    beginning email feature

---
+++ HooWantsBrunch/app/src/main/java/cs4720/cs/virginia/edu/hoowantsbrunch/EmailRestaurants.java
@@ -0,0 +1,13 @@
+package cs4720.cs.virginia.edu.hoowantsbrunch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmailRestaurants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_restaurants);
+    }
+}
